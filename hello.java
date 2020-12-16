3ff1abfa0a115391ecd9717feead2b2e09410a8e
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-12-16 02:58:20 +0000

    init

---
+++ hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * hello
+ */
+public class hello {
+
+  public static void main(String[] args) {
+    System.out.println("Hello EMILIO");
+
+    int myFirstNumber = 5;
+    System.out.println("My number is: " + myFirstNumber);
+
+  }
+}
