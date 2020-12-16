7e6ab652a84462f45753a6d9a8d18df94259dca2
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-12-16 05:54:32 +0000

    upd8

--- hello.java
+++ hello.java
@@ -5,17 +5,41 @@
 public class hello {
 
   public static void main(String[] args) {
-    System.out.println("Hello EMILIO");
+    System.out.println("JAVA Lessons");
 
-    int myFirstNumber = 5;
-    int mySecondNumber = 12;
-    int myThirdNumber = 6;
-    int myTotalNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+    // int myFirstNumber = 5;
+    // int mySecondNumber = 12;
+    // int myThirdNumber = 6;
+    // int myTotalNumber = myFirstNumber + mySecondNumber + myThirdNumber;
 
-    System.out.println("My total number is: " + myTotalNumber);
+    // System.out.println("My total number is: " + myTotalNumber);
 
-    int myLastOne = myTotalNumber - 1000;
-    System.out.println("Total number - 1000 = " + myLastOne);
+    // int myLastOne = myTotalNumber - 1000;
+    // System.out.println("Total number - 1000 = " + myLastOne);
+
+    // int myValue = 10000;
+
+    // int myMinValue = Integer.MIN_VALUE;
+    // int myMaxValue = Integer.MAX_VALUE;
+    // System.out.println(myValue);
+    // System.out.println("My minimum value " + myMinValue);
+    // System.out.println("My maximum value " + myMaxValue);
+
+    // byte myByteNumber = (byte) 150;
+    // short myShortNumber = (short) 99;
+
+    // int myInt = 2500;
+    // long myLong = (long) myInt + ((myByteNumber + myShortNumber + myInt) * 10);
+    // System.out.println("My long number: " + myLong);
+
+    float myMinFLoatValue = Float.MIN_VALUE;
+    float myMaxFloatValue = Float.MAX_VALUE;
+    System.out.println(myMinFLoatValue);
+    System.out.println(myMaxFloatValue);
+
+    int myIntValue = 5;
+    float myFloatValue = (float) 5.25;
+    double myDoubleValue = (double) 2.45;
 
   }
 }
