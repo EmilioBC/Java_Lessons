e7f65aac4a93c061490b3faa79be3197963f98fa
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2020-12-16 04:25:12 +0000

    Update

--- hello.java
+++ hello.java
@@ -8,7 +8,14 @@ public class hello {
     System.out.println("Hello EMILIO");
 
     int myFirstNumber = 5;
-    System.out.println("My number is: " + myFirstNumber);
+    int mySecondNumber = 12;
+    int myThirdNumber = 6;
+    int myTotalNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+
+    System.out.println("My total number is: " + myTotalNumber);
+
+    int myLastOne = myTotalNumber - 1000;
+    System.out.println("Total number - 1000 = " + myLastOne);
 
   }
 }
